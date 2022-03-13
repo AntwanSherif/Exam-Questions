e9093332532dcec906ec123bf82dc8fa1df52a68
AntwanSherif <antwansherif@gmail.com>
2022-03-13 22:08:05 +0200

    :sparkles: Add choice voting functionality

--- app/pages/questions/[questionId].tsx
+++ app/pages/questions/[questionId].tsx
@@ -3,12 +3,23 @@ import { Head, Link, useRouter, useQuery, useParam, BlitzPage, useMutation, Rout
 import Layout from "app/core/layouts/Layout"
 import getQuestion from "app/questions/queries/getQuestion"
 import deleteQuestion from "app/questions/mutations/deleteQuestion"
+import updateChoice from "app/choices/mutations/updateChoice"
 
 export const Question = () => {
   const router = useRouter()
   const questionId = useParam("questionId", "number")
+  const [updateChoiceMutation] = useMutation(updateChoice)
   const [deleteQuestionMutation] = useMutation(deleteQuestion)
-  const [question] = useQuery(getQuestion, { id: questionId })
+  const [question, { refetch }] = useQuery(getQuestion, { id: questionId })
+
+  const handleVote = async (id: number) => {
+    try {
+      await updateChoiceMutation({ id })
+      refetch()
+    } catch (error) {
+      alert("Error updating choice " + JSON.stringify(error, null, 2))
+    }
+  }
 
   return (
     <>
@@ -22,6 +33,7 @@ export const Question = () => {
           {question.choices.map((choice) => (
             <li key={choice.id}>
               {choice.text} - {choice.votes} votes
+              <button onClick={() => handleVote(choice.id)}>Vote</button>
             </li>
           ))}
         </ul>
